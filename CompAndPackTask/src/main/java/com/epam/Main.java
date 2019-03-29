18dcb9231370b239ffbba46239bf73897bdfe821
bear_i <rnmdvd@gmail.com>
2019-03-29 13:49:17 +0200

    CompAndPackTask

---
+++ CompAndPackTask/src/main/java/com/epam/Main.java
@@ -0,0 +1,11 @@
+package com.epam;
+
+import org.junit.Assert;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Medvid Iryna");
+
+        Assert.fail();
+    }
+}
